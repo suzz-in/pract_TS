dedab5efac7effd9b7ab3ec74b9091a89c994cbe
suzz-in <o_o_su_jin_@naver.com>
2022-12-19 14:50:16 +0900

    추상클래스를 인터페이스로 바꾸기

--- interface2.ts
+++ interface2.ts
@@ -1,13 +1,12 @@
 //추상 클래스는 상속받는 다른 클래스가 가져야 할 property랑 메소드를 명시 할 수 있도록 도와줌
-abstract class User {
-    constructor (
-        //protexted는 추상 클래스로부터 상속받은 클래스들이 property에 접근하도록 해줌
-        //private이면 접근할 수 없음
-        protected firstName : string,
-        protected lastName : string,
-    ){}
-    abstract sayHi(name: string): string
-    abstract fullName() : string
+interface User {
+    firstName: string,
+    lastName: string,
+    sayHi(name: string): string
+    fullName() : string
+}
+interface Human {
+    health: number
 }
 
 //추상 클래스는 그것으로부터 인스턴스를 만드는것 허용하지않음
@@ -16,11 +15,32 @@ abstract class User {
 //왜 추상클래스를 만드는가?
 // 표준화된 property와 메소드를 갖도록 해주는 청사진을 만들기 위해 추상클래스를 사용함
 //인터페이스는 가벼움. 인터페이스는 컴파일하면 js로 바뀌지 않고 사라짐
-class Player extends User {
-    getFullName(): string {
-        return `${this.firstName} ${this.lastName}`
-    }
-    sayHi(name: string){
-        return `hellow ${name}, my name is ${this.fullName()}`
-    }
+class Player implements User, Human {
+    //implements 사용하면 코드가 가벼워짐 - 컴파일 안되니까.. js는 없는 거라서
+    constructor(
+        // 인터페이스 상속할 때에는 property를 private로 만들지못함
+        public firstName: string,
+        public lastName : string,
+        public health : number,
+        ) {}
+        getFullName(): string {
+            return `${this.firstName} ${this.lastName}`
+        }
+        sayHi(name: string){
+            return `hellow ${name}, my name is ${this.fullName()}`
+        }
 }
+
+
+//인터페이스를 타입으로 사용가능
+//클래스를 타입으로 쓸 수 있고, 인터페이스도 타입으로 쓸 수 있음
+function makeUser(user: User) ={
+    return "hi"
+} 
+
+makeUser({
+    firstName: "nico",
+    lastName: "las",
+    fullName: ()=> "xx",
+    sayHi: (name) => "string"
+})
